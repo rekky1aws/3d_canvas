f439bf0920fc846690a9858158ff2fd90b9de69b
rekky1aws <killikomfr@gmail.com>
2025-12-31 05:57:26 +0100

    Rotating cube in 3 axis

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+ + Read objects from .obj
+ + Sliders to change :
+  + X translate
+  + Y translate
+  + Z translate
+ + Change Rotate to support all axis and a origin point (default {0, 0, 0})
---
+++ app.js
@@ -0,0 +1,218 @@
+/*
+(x, y, z)
+x' = x/z
+y' = y/z
+
+(x,y) = (0,0) = center of canvas
+z = 0 = viewer's eyese
+z = 1 = screen
+z > 1 = further than screen
+*/
+
+const BG = "#1D1D1D";
+const FG = "#66FF66";
+const FPS = 60;
+const DS = 20;
+
+let dzGlobal = 0;
+let dAngleGlobal = 0;
+
+canvas.width = 600;
+canvas.height = 600;
+
+const ctx = canvas.getContext("2d");
+
+// Draw
+function clearCanvas() {
+  ctx.fillStyle = BG;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawDot (dot) {
+  ctx.fillStyle = FG;
+  ctx.fillRect(dot.x - DS/2, dot.y - DS/2, DS, DS);
+}
+
+function drawProjectedDot (dot) {
+  const projDotSize = DS/dot.z;
+  ctx.fillStyle = FG;
+  ctx.fillRect(dot.x - projDotSize/2, dot.y - projDotSize/2, projDotSize, projDotSize);
+}
+
+function drawLine (dotA, dotB) {
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = FG;
+  ctx.beginPath();
+  ctx.moveTo(dotA.x, dotA.y);
+  ctx.lineTo(dotB.x, dotB.y);
+  ctx.stroke();
+}
+
+function drawPolyDots (poly) {
+  poly.dots.forEach (d => {
+    drawProjectedDot(coordsToScreen(project2D((d))));
+  });
+}
+
+function drawPolyLines (poly) {
+  poly.lines.forEach(l => {
+    drawLine(
+        coordsToScreen(project2D(poly.dots[l.a])),
+        coordsToScreen(project2D(poly.dots[l.b]))
+      );
+  });
+}
+
+// Maths
+function project2D (dot) { // Projects 3D point to 2D plan
+  return {
+    ...dot,
+    x: dot.x/dot.z,
+    y: dot.y/dot.z,
+  }
+}
+
+function coordsToScreen (dot) { // Takes normalized coordinates (-1 => 1) and transforms them to canvas coordinates (0 => canvas.size)
+  return {
+    ...dot,
+    x: ((dot.x + 1) / 2) * canvas.width,
+    y: (1 - ((dot.y + 1) / 2)) * canvas.height,
+  }
+}
+
+function rotateY (dot, angle = 0) {
+  return {
+    ...dot,
+    x: (dot.x*Math.cos(angle))-(dot.z*Math.sin(angle)),
+    z: (dot.x*Math.sin(angle))+(dot.z*Math.cos(angle))
+  }
+}
+
+function rotateX (dot, angle = 0) {
+  return {
+    ...dot,
+    y: (dot.y*Math.cos(angle))-(dot.z*Math.sin(angle)),
+    z: (dot.y*Math.sin(angle))+(dot.z*Math.cos(angle))
+  }
+}
+
+function rotateZ (dot, angle = 0) {
+  return {
+    ...dot,
+    x: (dot.y*Math.sin(angle))+(dot.x*Math.cos(angle)),
+    y: (dot.y*Math.cos(angle))-(dot.x*Math.sin(angle)),
+  }
+}
+
+function rotate (dot, angles = {x: 0, y: 0, z:0}) {
+  let resDot = {}; 
+
+  resDot = { // rotating around y axis
+    ...dot,
+    x: (dot.x*Math.cos(angles.y))-(dot.z*Math.sin(angles.y)),
+    z: (dot.x*Math.sin(angles.y))+(dot.z*Math.cos(angles.y))
+  };
+
+  resDot = { // rotating around x axis
+    ...resDot,
+    y: (resDot.y*Math.cos(angles.x))-(resDot.z*Math.sin(angles.x)),
+    z: (resDot.y*Math.sin(angles.x))+(resDot.z*Math.cos(angles.x))
+  }
+
+  resDot = { // rotating around z axis
+    ...resDot,
+    x: (resDot.y*Math.sin(angles.z))+(resDot.x*Math.cos(angles.z)),
+    y: (resDot.y*Math.cos(angles.z))-(resDot.x*Math.sin(angles.z)),
+  };
+
+  return resDot;
+}
+
+function translate (dot, delta = {x: 0, y: 0, z: 0}) {
+  return {
+    x: dot.x + delta.x,
+    y: dot.y + delta.y,
+    z: dot.z + delta.z,
+  }
+}
+
+// Maths Poly
+function polyRotateY (poly, angle = 0) {
+  let dots = [];
+  poly.dots.forEach(d => {
+    dots.push(rotateY(d, angle));
+  });
+
+  return {
+    ...poly,
+    dots
+  };
+}
+
+function polyRotateX (poly, angle = 0) {
+  let dots = [];
+  poly.dots.forEach(d => {
+    dots.push(rotateX(d, angle));
+  });
+
+  return {
+    ...poly,
+    dots
+  };
+}
+
+function polyRotateZ (poly, angle = 0) {
+  let dots = [];
+  poly.dots.forEach(d => {
+    dots.push(rotateZ(d, angle));
+  });
+
+  return {
+    ...poly,
+    dots
+  };
+}
+
+function polyRotate (poly, angles = {x: 0, y: 0, z: 0}) {
+  let dots = [];
+  poly.dots.forEach(d => {
+    dots.push(rotate(d, angles));
+  });
+
+  return {
+    ...poly,
+    dots
+  };
+}
+
+function polyTranslate (poly, delta = {x: 0, y: 0, z: 0}) {
+  let dots = [];
+  poly.dots.forEach(d => {
+    dots.push(translate(d, delta));
+  });
+
+  return {
+    ...poly,
+    dots
+  };
+}
+
+
+// Animation
+function frame () {
+  clearCanvas();
+  dzGlobal += 1/FPS;
+  dAngleGlobal += 1/FPS;
+  // drawPolyLines(polyTranslate(polyRotateY(cube3, dAngleGlobal), 0, 0, 1.5));
+  // drawPolyLines(polyTranslate(polyRotateX(cube3, dAngleGlobal), 0, 0, 1.5));
+  // drawPolyLines(polyTranslate(polyRotateZ(cube3, dAngleGlobal), 0, 0, 1.5));
+
+  // drawPolyLines(polyTranslate(polyRotateZ(polyRotateX(polyRotateY(cube3, dAngleGlobal), dAngleGlobal), dAngleGlobal), 0, 0, 1.5));
+
+  drawPolyLines(polyTranslate(polyRotate(cube3, {x: dAngleGlobal, y: dAngleGlobal, z:dAngleGlobal}), {x: 0, y: 0, z: 1.25}));
+}
+
+
+// MAIN
+// drawPolyLines(polyTranslate(cube3, 0, 0, 0.4));
+setInterval(frame, 1000/FPS);
